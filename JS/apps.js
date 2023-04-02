ec96a3eee58580060549fd3266cec36902c1e2df
Hunter Eckhoff <Phantazm777@verizon.net>
2023-04-02 18:46:26 -0400

    add animated gifs of knight and 1 monster

--- JS/apps.js
+++ JS/apps.js
@@ -10,26 +10,54 @@ let gameLevel, win, playerHP, computerHp, combatLog, turn, startFight, actionMen
 /*------------------------ Cached Element References ------------------------*/
 const createStartMenuImg = document.createElement("img")
 createStartMenuImg.className = "start-img"
+
+
 const createStartButtonEl = document.createElement("button")
 createStartButtonEl.className = "start-button"
 createStartButtonEl.innerText = "Start Game!"
+
+
 const createContinueButtonEl = document.createElement("button")
 createContinueButtonEl.className = "continue-button"
 createContinueButtonEl.innerText = "Continue"
+
+
 const startScreenEl = document.querySelector(".start-screen")
+
+
 const firstMessageScreenEl = document.querySelector(".first-message")
+
+
 const menuTitleEl = document.createElement("h1")
 menuTitleEl.className = "menu-title"
 menuTitleEl.innerText = "Starry Knight"
+
+
 const createFirstBattleImg = document.createElement("img")
+
+
 const createKnightImg = document.createElement("img")
 createKnightImg.className = "knight"
+
+
 const battleScreenEl = document.querySelector(".battle-screen")
-/*----------------------------- Event Listeners -----------------------------*/
+
+
+const stoneGolemGif = document.createElement("img")
+stoneGolemGif.className = "golem"
+/*----------------------------- Event Listeners ------------------------------*/
 
 createContinueButtonEl.addEventListener("click", disableFirstMessageScreen)
 createStartButtonEl.addEventListener("click", disableMainScreen)
-/*-------------------------------- Functions --------------------------------*/
+/*-------------------------------- Objects -----------------------------------*/
+let player = {hp: 100, turn: 1, stars: 0}
+
+let enemy1 = {hp: 100, turn: -1, stars: 10}
+let enemy2 = {hp: 100, turn: -1, stars: 20}
+let enemy3 = {hp: 100, turn: -1, stars: 30}
+let enemy4 = {hp: 100, turn: -1, stars: 40}
+let enemy5 = {hp: 100, turn: -1, stars: 50}
+/*-------------------------------- Functions ---------------------------------*/
 function init() {
     startGameMenu();
     gameLevel = 1;
@@ -44,41 +72,42 @@ function init() {
 }
 init()
 
-// Fight() {
-//     gameLevel = updateGameLevel()
-//     win = false
-//     playerHP = 100
-//     computerHp = 100
-//     combatLog = empty
-//     turn = 1   //maybe random later
-//     startFight = true
-//     actionMenu = true
-//     stars = updateStars()
-// }
 
 
-// render() {
-//     Fight()
-//     playerChoice()
-//     computerChoice()
-//     dmgCalculations()
-//     updateCombatLog()
-//     updateHp()
-//     checkIfWin() {
-//         if (win) {
-//             updateStars()
-//             winStatusMessage/effects()
+render() {
+    fight()
+    playerChoice()
+    computerChoice()
+    dmgCalculations()
+    updateCombatLog()
+    updateHp()
+    checkIfWin() {
+        if (win) {
+            updateStars()
+            winStatusMessage/effects()
+            
+            
+            //then onclick
+            menusInbetweenFights() {
+                fight()
+            }
+        }
+    updateTurn()
+    }
+}
+                
+function fight() {
+    gameLevel = updateGameLevel()
+    win = false
+    playerHP = 100
+    computerHp = 100
+    combatLog = empty
+    turn = 1   //maybe random later
+    startFight = true
+    actionMenu = true
+    stars = updateStars()
 
-
-//             //then onclick
-//             menusInbetweenFights() {
-//                 initFight()
-//             }
-//         }
-//     updateTurn()
-//     }
-// }  
-
+}
 function startGameMenu() {
     createStartMenuImg.src="../css/Wallpaper-Shovel-Knight-Video-Games-Pixel-Art-Retro-Gam50.jpg"
     startScreenEl.append(createStartMenuImg)
@@ -100,8 +129,10 @@ function disableFirstMessageScreen(evnt) {
     firstMessageScreenEl.remove()
     createFirstBattleImg.src="../css/CrystalCave1-1920x1080-2a8443ca448c40ef77c4da5d220c5e23.jpg"
     battleScreenEl.append(createFirstBattleImg)
-    createKnightImg.src="../css/knight-for-project-flipped.png"
+    createKnightImg.src="../css/knight-animation.gif"
     battleScreenEl.append(createKnightImg)
+    stoneGolemGif.src="../css/output-onlinegiftools (1).gif"
+    battleScreenEl.append(stoneGolemGif)
     evnt.stopPropagation()
 }
 
